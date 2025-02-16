91d273112ae2a4f1e6caad41e1b95e892523070f
Mchikhi59 <Amara1959@>
2025-02-16 12:39:23 -0500

    ubdate

---
+++ src/main/java/org/example/E9secondLargestNumber.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class E9secondLargestNumber {
+    public static void main(String[] args) {
+        int [] numbers={10,12,42,85,34,96,10,52,};
+        int largest=numbers[0];
+        int secondLargest=numbers[0];
+
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest) {
+                secondLargest = largest; // Update second largest
+                largest = numbers[i]; // Update largest
+            } else if (numbers[i] > secondLargest && numbers[i] != largest) {
+                secondLargest = numbers[i]; // Update second largest
+            }
+
+        }
+        System.out.println("The second largest number is: " + secondLargest);
+
+
+    }
+}
